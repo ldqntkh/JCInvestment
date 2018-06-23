387408261a1403a98fd37e395b95f2a9205f999f
Quang.Le <ldqntkh@gmail.com>
2018-06-23 09:56:27 +0700

    first config project

---
+++ myBitboxMining/webnode-mining/app.js
@@ -0,0 +1,88 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var session = require("express");
+var csrf = require('csurf');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: {
+        maxAge: 1000 * 60 * 30
+    },
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+passport.use(new LocalStrategy(
+    async function (username, password, done) {
+        // var userMgr = new userManager(app.locals._db);
+        // var user = await userMgr.getUserByEmailAndPassword(username, password);
+        // if (user !== null) {
+        //   return done(null, user);
+        // }
+        // else {
+        //   return done(null, false, {message: 'Incorrect password.'})
+        // }
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    //done(null, user.getUserId());
+});
+
+passport.deserializeUser(async function (id, done) {
+    // var userMgr = new userManager(app.locals._db);
+    // var user = await userMgr.getUserById(id);
+    // if (user !== null) {
+    //   return done(null, user.getUserId());
+    // }
+    // else {
+    //   return done(null, false, {message: 'Incorrect password.'})
+    // }
+});
+
+module.exports = app;
