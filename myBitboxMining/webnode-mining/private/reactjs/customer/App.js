4488520b19fc0b291ce3c6fa487e26a963f9ebe0
Quang.Le <ldqntkh@gmail.com>
2018-06-28 00:16:12 +0700

    [client-reactjs] make component show list product

--- myBitboxMining/webnode-mining/private/reactjs/customer/App.js
+++ myBitboxMining/webnode-mining/private/reactjs/customer/App.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 // import component
 import IndexEthExchangeComponent from './components/dashboard/indexEthExchangeComponent';
+import ListProductComponent from './components/products/listProductComponent';
 import CoinCalculator from './components/calculation/coinCalculator';
+
 if (typeof pageContext !== 'undefined') {
     let page = pageContext.page;
     if (page === 'dashboard') {
         ReactDOM.render(<IndexEthExchangeComponent />, document.getElementById('eth-exchange'));
+        ReactDOM.render(<ListProductComponent />, document.getElementById('list-product'));
     } else if (page === 'calculation') {
         ReactDOM.render(<CoinCalculator />, document.getElementById('calculation_hashing'));
     }
---
+++ myBitboxMining/webnode-mining/private/reactjs/customer/components/products/listProductComponent.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+
+// import component
+import ProductItemComponent from './productItemComponent';
+
+// import variable
+import { API_URL } from '../../const/variable';
+
+
+export default class ListProductComponent extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            loaded: false,
+            listProduct: []
+        }
+    }
+
+    componentDidMount() {
+        this._getListProduct();
+    }
+
+    async _getListProduct () {
+        let url = API_URL + 'products/list';
+        try {
+            let response = await fetch(url);
+            let jsonData = await response.json();
+            if (jsonData.status === 'success') {
+                this.setState({
+                    loaded: true,
+                    listProduct: jsonData.data
+                });
+            } else {
+                this.setState({
+                    loaded: true
+                })
+                console.log(jsonData.errMessage);
+            }
+        } catch (err) {
+            this.setState({
+                loaded: true
+            })
+            console.log(err.errMessage);
+        }
+    } 
+
+    render () {
+        let screen = null;
+        if (!this.state.loaded) screen = <i className="fa fa-spinner fa-spin fa-icon-loading"></i>
+        else {
+            screen = this.state.listProduct.map((item, index)=> {
+                return <ProductItemComponent dataProduct={item} key={index} />;
+            });
+        }
+        return (
+            screen
+        );
+    }
+}
